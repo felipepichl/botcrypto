6666c91f18f34291ab8e045e741a3de528a27243
Felipe Pichl <felipe.pichl@hotmail.com>
2022-06-09 19:23:39 -0300

    Bot-3.0: RSI calc

--- version-1.0/src/app/controllers/BotController.js
+++ version-1.0/src/app/controllers/BotController.js
@@ -4,9 +4,30 @@ class BotController {
   async index(req, res) {
     const response = await api.get('');
 
-    const candle = response.data[499];
+    const closes = response.data.map(candle => parseFloat(candle[4]));
 
-    return res.send(candle[4]);
+    // eslint-disable-next-line no-unused-vars
+    let gains = 0;
+    // eslint-disable-next-line no-unused-vars
+    let losses = 0;
+
+    // eslint-disable-next-line no-plusplus
+    for (let i = closes.length - 14; i < closes.length; i++) {
+      // eslint-disable-next-line no-multi-assign
+      const diff = closes[i] - closes[i - 1];
+
+      if (diff >= 0) {
+        gains += diff;
+      } else {
+        losses -= diff;
+      }
+    }
+
+    const strength = gains / losses;
+
+    const result = 100 - 100 / (1 + strength);
+
+    return res.send({ rsi: result });
   }
 }
 
